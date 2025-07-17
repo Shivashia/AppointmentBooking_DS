5dd798fc2aa5acd55ae57601d601edad5f829739
Shiv Avashia <42934934+Shivashia@users.noreply.github.com>
2025-07-17 11:19:55 +0530

    Cloud Config_NOT_WORKING
    FTS

--- Patient/src/main/java/com/Devup/Patient/PatientApplication.java
+++ Patient/src/main/java/com/Devup/Patient/PatientApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class PatientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientApplication.class, args);
+	}
+
+	@PostConstruct
+	public void printDataSourceInfo() {
+		System.out.println("Datasource URL = " + System.getProperty("spring.datasource.url"));
 	}
 
 }
